1aa5b9b9acb9452f28cf30ef11a08381924a2754
Shivaraj B H <sbh69840@gmail.com>
2022-02-19 10:05:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,28 @@
 from flask import Flask, jsonify, request  # import objects from the Flask model
-from keras.models import load_model
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
-# import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
 app = Flask(__name__)  # define app using Flask
 
-tweets = [{'content': 'dont know what todo', 'date': '17-02-2021', 'id': '1'},
-          {'content': 'doing flask and its pretty great', 'date': '18-02-2021', 'id': '2'}]
+model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B")
+tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
 
-# print(keras.__version__)
-# model = load_model('./tf_model.h5')
-
-# NO PYTORCH
-tokenizer = AutoTokenizer.from_pretrained(
-    "distilbert-base-uncased-finetuned-sst-2-english")
-
-model = AutoModelForSequenceClassification.from_pretrained(
-    "distilbert-base-uncased-finetuned-sst-2-english")
-
-pipe = TextClassificationPipeline(model=model, tokenizer=tokenizer)
-
-
-@app.route('/', methods=['GET'])
-def test():
-    content = request.json['content']
-
-    hasil = pipe(content)
-    print(hasil[0]['label'])
-    return jsonify({'message': content, 'sentiment': hasil[0]['label']})
-
-
-@app.route('/tweet', methods=['GET'])
-def returnAll():
-    return jsonify({'tweets': tweets})
-
-
-@app.route('/tweet/<string:date>', methods=['GET'])
-def returnOne(date):
-    tweet = [t for t in tweets if t['date'] == date]
-    return jsonify({'data': tweet[0]})
-
-
-@app.route('/tweet', methods=['POST'])
-def createTweet():
-    content = request.json['content']
-    hasil = pipe(content)
-
-    tweet = {'content': content,
-             'date': request.json['date'], 'id': request.json['id'], 'sentiment': hasil[0]['label']}
-
-    tweets.append(tweet)
-    return jsonify({'data': tweet})
-
-
-@app.route('/tweet/<string:id>', methods=['PUT'])
-def editOne(id):
-    tweet = [t for t in tweets if t['id'] == id]
-    tweet[0]['content'] = request.json['content']
-    return jsonify({'data': tweet[0]})
-
-
-@app.route('/delete-tweet/<string:id>', methods=['DELETE'])
-def removeOne(id):
-
-    tweet = [t for t in tweets if t['id'] == id]
-    tweets.remove(tweet[0])
-    return jsonify({'tweets': tweets})
-
+def predict(context,temperature,max_length):
+    input_ids = tokenizer(context, return_tensors="pt").input_ids
+    gen_tokens = model.generate(
+        input_ids,
+        do_sample=True,
+        temperature=temperature,
+        max_length=max_length,
+    )
+    gen_text = tokenizer.batch_decode(gen_tokens)
+    print(len(gen_text))
+    return gen_text[0]
+@app.route('/complete', methods=['POST'])
+def generate_text():
+    context = request.json['context']
+    temperature = request.json['temperature']
+    max_length = request.json['max_length']
+    res = predict(context,temperature,max_length)
+    return jsonify({'output': res})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)  # run app on port 8080 in debug mode
+    app.run(debug=False, port=5000)  # run app on port 8080 in debug mode
